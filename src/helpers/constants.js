35025285313d3fc4044f4c67014c48dfbf235b05
Demian <devaccdemiann@gmail.com>
2021-08-09 00:28:14 +0200

    Add constants

---
+++ src/helpers/constants.js
@@ -0,0 +1,58 @@
+/**
+ * @description The base url for the Joke API
+ */
+const BASE = 'https://v2.jokeapi.dev';
+
+/**
+ * @description The host of the Joke API
+ */
+const HOST = 'https://jokeapi.dev';
+
+/**
+ * @description The current version for the JokeAPI API
+ */
+const API_VERSION = '2';
+
+/**
+ * @description The current categories avaliable
+ */
+const CATEGORIES = [
+  'any',
+  'misc',
+  'programming',
+  'dark',
+  'pun',
+  'spooky',
+  'christmas',
+  'miscellaneous',
+  'coding',
+  'development',
+  'halloween',
+];
+
+/**
+ * @description The current blacklist flags avaliable
+ */
+const BLACKLIST_FLAGS = ['nsfw', 'religious', 'political', 'racist', 'sexist', 'explicit'];
+
+/**
+ * @description The current formats avaliable
+ */
+const FORMAT = ['json', 'xml', 'yaml', 'txt'];
+
+/**
+ * @description The current joke outputs avaliable
+ */
+const TYPE = ['single', 'twopart'];
+
+/**
+ * @description Current max amount of jokes
+ */
+const AMOUNT_MAX = 10;
+
+/**
+ * @description Request timeout
+ */
+const TIMEOUT = 30000;
+
+module.exports = { BASE, HOST, API_VERSION, CATEGORIES, BLACKLIST_FLAGS, FORMAT, TYPE, AMOUNT_MAX, TIMEOUT };
