a1fd2a9854f663c37c615c02cef65965ff0b001c
Demian <devaccdemiann@gmail.com>
2021-08-09 16:15:16 +0200

    Update example file

---
+++ index.example.js
@@ -0,0 +1,33 @@
+const JokeAPI = require('.');
+const JokeClient = new JokeAPI();
+
+const get = async () => {
+  const joke = await JokeClient.getJoke();
+  console.log(joke);
+
+  const info = await JokeClient.info({ lang: 'en' });
+  console.log(info);
+
+  const categories = await JokeClient.info();
+  console.log(categories);
+
+  const langcode = await JokeClient.langcode({ language: 'eng' });
+  console.log(langcode);
+
+  const languages = await JokeClient.langcode();
+  console.log(languages);
+
+  const flags = await JokeClient.flags();
+  console.log(flags);
+
+  const formats = await JokeClient.formats();
+  console.log(formats);
+
+  const ping = await JokeClient.ping();
+  console.log(ping);
+
+  const endpoints = await JokeClient.endpoints();
+  console.log(endpoints);
+};
+
+get();
