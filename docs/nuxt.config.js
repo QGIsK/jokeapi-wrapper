38543a84bdae412ac922ab8b6d8c0cbab2c065be
Demian <devaccdemiann@gmail.com>
2021-11-21 16:04:05 +0100

    chore: add plausible

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,7 +1,17 @@
-import theme from '@nuxt/content-theme-docs'
+import theme from '@nuxt/content-theme-docs';
 
 export default theme({
+  head: {
+    script: [
+      {
+        async: true,
+        defer: true,
+        'data-domain': 'jokewrapper.demiann.dev',
+        src: 'https://analytics.demiann.dev/js/plausible.js',
+      },
+    ],
+  },
   docs: {
-    primaryColor: '#E24F55'
-  }
-})
+    primaryColor: '#E24F55',
+  },
+});
